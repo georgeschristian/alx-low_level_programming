d9840ea2de2705d71a4af07619fa4f8cb14c13a2
georgeschristian <mbogeorges@gmail.com>
2022-12-20 11:37:37 -0800

    reset

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
- * _strlen - function
- * @s:char printer
- * Description:a function that returns the length of a string
- * Returns:leng of @s or 0 if @s is null
+ *_strlen - function
+ *@s:char printer
+ *Description:a function that returns the length of a string
+ *Returns:leng of @s or 0 if @s is null
  */
 int _strlen(char *s)
 {
 	int i = 0;
+
 	if (s == NULL)
 		return (0);
 	while (s[i] != '\0')

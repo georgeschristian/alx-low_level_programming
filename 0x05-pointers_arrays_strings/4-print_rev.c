ae7e2690667c83162c7b3215c90401ba48c283d8
georgeschristian <mbogeorges@gmail.com>
2022-12-20 12:36:31 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * rev_string -function
+ * @s:pointer
+ * Description:a function that reverses a string.
+ * Return:nothing
+ */
+void print_rev(char *s)
+{
+	if (s != NULL)
+	{
+		int t = 0;
+		int i;
+
+		while (s[t] != '\0')
+			t++;
+		i = t - 1;
+
+		while (i >= 0)
+		{
+			_putchar(s[i]);
+			i--;
+		}
+		_putchar('\n');
+	}
+}

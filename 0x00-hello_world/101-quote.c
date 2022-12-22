7f6eaef9090b7467087d7e6ebe056f5c022737a1
georgeschristian <mbogeorges@gmail.com>
2022-12-22 02:49:07 -0800

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +9,8 @@
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+
 	return (1);
 }

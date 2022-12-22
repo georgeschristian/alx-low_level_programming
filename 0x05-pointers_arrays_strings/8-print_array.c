b29ff3f3ccae3232324332ecbf0c1867e517c0c3
georgeschristian <mbogeorges@gmail.com>
2022-12-21 23:53:49 -0800

    my commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,9 @@ void print_array(int *a, int n)
 
 		for (i = 0; i < n; i++)
 		{
-			print("%d", a[i]);
+			printf("%d", a[i]);
 			if (i != n - 1)
-				print(", ");
+				printf(", ");
 		}
 	}
 	printf("\n");

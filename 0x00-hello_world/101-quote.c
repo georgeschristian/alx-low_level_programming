ec1e39e7159faf26d31ea9a424d3e25b5359dd84
georgeschristian <mbogeorges@gmail.com>
2022-12-22 02:43:49 -0800

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:Always 1 (Succcess)
+ *
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1)
+}

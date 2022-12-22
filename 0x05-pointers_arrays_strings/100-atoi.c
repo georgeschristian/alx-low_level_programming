4ec4495b21ce45fde618596b1c00321943e81bcf
georgeschristian <mbogeorges@gmail.com>
2022-12-22 00:17:29 -0800

    my commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_atoi -function
+ *@s:char pointer
+ *Description:a function that a string to an integer
+ *Return:integer
+ */
+int _atoi(char *s)
+{
+	int sig = 1;
+	int i = 0;
+	unsigned int res = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sig *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0')
+			i++;
+	}
+	res *= sig;
+	return (res);
+}
